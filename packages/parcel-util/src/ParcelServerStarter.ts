2705b527f3c2f4296b904bd5d04823f18a72a6bc
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2020-11-27 21:55:00 +0800

    [FEATURE]: parcel-util: Wip ParcelServerStarter

--- packages/parcel-util/src/ParcelServerStarter.ts
+++ packages/parcel-util/src/ParcelServerStarter.ts
@@ -1,5 +1,52 @@
+import Parcel from "@parcel/core";
+import type {FileSystem} from "@parcel/fs";
+import {NodeFS} from "@parcel/fs";
+import type {PackageManager} from "@parcel/package-manager";
+import {NodePackageManager} from "@parcel/package-manager";
+import type {InitialParcelOptions} from "@parcel/types";
+
+interface ParcelServerStarterOptions extends Omit<InitialParcelOptions, "packageManager"> {}
+
 export class ParcelServerStarter {
+    private readonly fs: FileSystem;
+    private readonly packageManager: PackageManager;
+    private readonly parcel: Parcel;
+
+    private unsubscribe?: () => Promise<any>;
+    private isExiting = false;
+
+    constructor(options: ParcelServerStarterOptions) {
+        this.fs = new NodeFS();
+        this.packageManager = new NodePackageManager(this.fs);
+        this.parcel = new Parcel({
+            packageManager: this.packageManager,
+            ...options,
+        });
+    }
+
     run() {
-        // TODO
+        this.parcel
+            .watch((err, buildEvent) => {
+                if (err) {
+                    throw err;
+                }
+            })
+            .then(_ => {
+                this.unsubscribe = _.unsubscribe;
+            });
+        process.stdin.on("end", this.exit);
+        process.on("SIGINT", this.exit);
+        process.on("SIGTERM", this.exit);
+    }
+
+    private async exit(exitCode: number = 0) {
+        if (this.isExiting) {
+            return;
+        }
+        this.isExiting = true;
+        if (this.unsubscribe !== undefined) {
+            await this.unsubscribe();
+        }
+        process.exit(exitCode);
     }
 }
---
+++ packages/parcel-util/src/global-types.d.ts
@@ -0,0 +1,82 @@
+declare module "@parcel/core" {
+    export default class Parcel {
+        constructor(options: object);
+
+        watch(cb?: (err?: Error | null | undefined, buildEvent?: any) => any): Promise<import("@parcel/types").AsyncSubscription>;
+    }
+}
+
+declare module "@parcel/fs" {
+    export interface FileSystem {}
+    export class NodeFS implements FileSystem {}
+}
+
+declare module "@parcel/package-manager" {
+    export interface PackageManager {}
+    export class NodePackageManager implements PackageManager {
+        constructor(fs: import("@parcel/fs").FileSystem, installer?: any);
+    }
+}
+
+declare module "@parcel/types" {
+    export type FilePath = string;
+    export type SemverRange = string;
+    export type ModuleSpecifier = string;
+    export type HMROptions = {port?: number; host?: string; [_: string]: unknown};
+    export type BuildMode = "development" | "production" | string;
+    export type LogLevel = "none" | "error" | "warn" | "info" | "verbose";
+    export type InitialServerOptions = {
+        publicUrl?: string;
+        host?: string;
+        port: number;
+        https?: HTTPSOptions | boolean;
+    };
+    export type HTTPSOptions = {cert: FilePath; key: FilePath};
+    export type ResolvedParcelConfigFile = {[_: string]: any};
+    export type Engines = {
+        browsers?: string | Array<string>;
+        electron?: SemverRange;
+        node?: SemverRange;
+        parcel?: SemverRange;
+        [_: string]: any;
+    };
+
+    export type InitialParcelOptions = {
+        entries?: FilePath | Array<FilePath>;
+        rootDir?: FilePath; // TODO: Renamed to `entryRoot` at HEAD
+        // entryRoot?: FilePath;
+        config?: ResolvedParcelConfigFile; // TODO: Changed to type `ModuleSpecifier` at HEAD
+        // config?: ModuleSpecifier;
+        defaultConfig?: ResolvedParcelConfigFile; // TODO: Changed to type `ModuleSpecifier` at HEAD
+        // defaultConfig?: ModuleSpecifier;
+        env?: {[key: string]: string | void};
+        targets?: Array<string> | {[_: string]: any} | null | undefined;
+
+        disableCache?: boolean;
+        cacheDir?: FilePath;
+        killWorkers?: boolean;
+        mode?: BuildMode;
+        scopeHoist?: boolean;
+        sourceMaps?: boolean;
+        publicUrl?: string;
+        distDir?: FilePath;
+        hot?: HMROptions | null | undefined;
+        contentHash?: boolean;
+        serve?: InitialServerOptions | false;
+        autoinstall?: boolean;
+        logLevel?: LogLevel;
+        profile?: boolean;
+        patchConsole?: boolean;
+
+        inputFS?: import("@parcel/fs").FileSystem;
+        outputFS?: import("@parcel/fs").FileSystem;
+        workerFarm?: any;
+        packageManager?: import("@parcel/package-manager").PackageManager;
+        defaultEngines?: Engines;
+        detailedReport?: number | boolean;
+    };
+
+    export interface AsyncSubscription {
+        unsubscribe(): Promise<any>;
+    }
+}
